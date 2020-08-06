70ab631ac89621cdbf47c566b6eebd1e056d41ab
Ahmed Sadek <62098165+ahsadek93@users.noreply.github.com>
2020-08-06 17:15:14 +0200

    Updates

---
+++ precache-manifest.890a2a0d7fd32025be4ef22957ee6cfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c148c33a4ee1776367a7c436e94825aa",
+    "url": "/arooh-dashboard/index.html"
+  },
+  {
+    "revision": "1bd1523b9206d1aad3dd",
+    "url": "/arooh-dashboard/static/css/main.366c51f9.chunk.css"
+  },
+  {
+    "revision": "a7c94c3b53e6e3cabc76",
+    "url": "/arooh-dashboard/static/js/2.a1542616.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/arooh-dashboard/static/js/2.a1542616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd1523b9206d1aad3dd",
+    "url": "/arooh-dashboard/static/js/main.23e8d974.chunk.js"
+  },
+  {
+    "revision": "05b02a953a761c6fc3df",
+    "url": "/arooh-dashboard/static/js/runtime-main.ebcf7a18.js"
+  },
+  {
+    "revision": "ea487fe1509bf10dab164185391c6aa6",
+    "url": "/arooh-dashboard/static/media/sideView01.ea487fe1.png"
+  }
+]);
